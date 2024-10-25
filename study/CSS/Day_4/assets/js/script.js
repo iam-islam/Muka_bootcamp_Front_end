f8a00e51a49ac12b60089266e985c85abbda12e5
Islam <iamislam.fsd@gmail.com>
2024-10-25 20:22:24 +0300

    Day - 2

--- study/CSS/Day_4/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-/***************************************************
- * Project name     : .                            *
- * Author           : Islam.                       *
- * Version          : 1.0.                         *
- * File description : Main JS file.                *
- ***************************************************/
-
-console.log("Hello, world!");
